53e8642cc0879cd7f25a610cbce94172776b3fd4
dunbar-brett <dunbar.brett@gmail.com>
2021-01-31 22:34:59 -0800

    some material design stuff

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import AppBar from '@material-ui/core/AppBar';
+import ToolBar from '@material-ui/core/ToolBar';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/core/Menu';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>
-          Water Your Plants
-        </h1>
-      </header>
+      <AppBar>
+        <ToolBar>
+          <IconButton  edge="start" color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <h1>Water Your Plants</h1>
+        </ToolBar>
+      </AppBar>
     </div>
   );
 }

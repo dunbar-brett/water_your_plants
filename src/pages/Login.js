4db69d8dc742df56134f2d777399c411adf7d0a5
dunbar-brett <dunbar.brett@gmail.com>
2021-04-19 23:14:28 -0700

    added react hook forms. created start of login form

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,28 @@
 import React from 'react';
 
+import { useForm } from "react-hook-form";
 export default function Login() {
+  const { register, handleSubmit } = useForm();
+  
+  function onSubmit(data) {
+    console.log(data);
+  }
 
   return (
     <>
-    <h1>I'm the login page</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <label htmlFor='email'>Email: </label>
+          <input type='email' id='email' {...register('email')}/>
+        </div>
+        <div>
+          <label htmlFor='password'>Password: </label>
+          <input id='password' type='password' {...register('password')}/>
+        </div>
+        <button type='submit'>
+            Submit
+        </button>
+      </form>
     </>
   );
 }

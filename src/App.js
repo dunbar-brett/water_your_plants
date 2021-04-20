c7b8390561cafd469a36b233d37bff144d245e22
dunbar-brett <dunbar.brett@gmail.com>
2021-04-19 22:46:56 -0700

    new react app. starting with login form. going to install react hook forms

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Login from './pages/Login';
+
+function App() {
+  return (
+    <div className="App">
+      {/* nav bar goes here */}
+      <Login></Login>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Login.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Login() {
+
+  return (
+    <>
+    <h1>I'm the login page</h1>
+    </>
+  );
+}
